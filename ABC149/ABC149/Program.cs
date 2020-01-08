d4d758a12f26c01777cb35832bddd4c8226c4b97
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-08 23:30:09 +0900

    0108s practice

---
+++ ABC149/ABC149/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ABC149
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] inp = Console.ReadLine().Split(' ');
+            Console.WriteLine(inp[1] + inp[0]);
+        }
+    }
+}
---
+++ ABC149/ABC149B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ABC149B
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            long[] inp = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            string result;
+            if (inp[0] + inp[1] <= inp[2])
+            {
+                result = "0 0";
+            }
+            else
+            {
+                if (inp[0] >= inp[2])
+                {
+                    inp[0] -= inp[2];
+                }
+                else
+                {
+                    inp[1] -= inp[2] - inp[0];
+                    inp[0] = 0;
+                }
+                result = inp[0].ToString() + ' ' + inp[1].ToString();
+            }
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
