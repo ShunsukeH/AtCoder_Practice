851abb9b4fd81c00b333d3f329798dd72b63977e
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-12 18:09:13 +0900

    ABC150

---
+++ ABC150/ABC150A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ABC150A
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] inp = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine(500 * inp[0] >= inp[1] ? "Yes" : "No");
+        }
+    }
+}
---
+++ ABC150/ABC150B/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+namespace ABC150B
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Replace("ABC", "0").Where(c => c == '0').Count());
+        }
+    }
+}
