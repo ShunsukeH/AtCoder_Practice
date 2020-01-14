367ddc184de6eb73022e7cbf63d99b024b9f6c48
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-14 22:37:45 +0900

    add

---
+++ ABC148/ABC148B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ABC148B
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] st = Console.ReadLine().Split();
+            char[] s = st[0].ToCharArray();
+            char[] t = st[1].ToCharArray();
+
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += s[i].ToString() + t[i].ToString();
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
