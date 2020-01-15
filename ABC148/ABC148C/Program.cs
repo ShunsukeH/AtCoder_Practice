5437a411a230894c1f9fc47cdbb72ad2ae0885b7
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-15 22:19:48 +0900

    ABC148C

---
+++ ABC148/ABC148C/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ABC148C
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            long[] inp = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+            long x = inp[0] * inp[1];
+            if (inp[1] > inp[0])
+            {
+                long tmp = inp[0];
+                inp[0] = inp[1];
+                inp[1] = tmp;
+            }
+
+            long r = inp[0] % inp[1];
+            while (r!=0)
+            {
+                inp[0] = inp[1];
+                inp[1] = r;
+                r = inp[0] % inp[1];
+            }
+            Console.WriteLine(x / inp[1]);
+        }
+    }
+}
