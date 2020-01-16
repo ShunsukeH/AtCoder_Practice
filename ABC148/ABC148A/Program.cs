fbde9ef47034d35ebe9ce73ca383e6c8a3a6b8e5
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-16 20:40:46 +0900

    ABC148A

---
+++ ABC148/ABC148A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ABC148A
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int[] ans = { 1, 2, 3 };
+            foreach (var i in ans)
+            {
+                if (i != a && i != b)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
