3fe48b8f5c29c8bf2afae54cbb2d18c7e6259c36
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-17 23:43:05 +0900

    ABC147AB

---
+++ ABC147/ABC147A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+namespace ABC147A
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int[] inp = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine(inp.Sum() > 21 ? "bust" : "win");
+        }
+    }
+}
---
+++ ABC147/ABC147B/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace ABC147B
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] a = s.ToCharArray();
+            char[] b = s.Reverse().ToArray();
+
+            int cnt = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    b[i] = a[i];
+                    a[s.Length - (i + 1)] = b[i];
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
