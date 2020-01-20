c263348dae22e839c84d4c2a47714aa7d0998ae7
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-20 21:57:38 +0900

    ABC152D solved

---
+++ ABC152/ABC152D/EmptyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ABC152D
+{
+    public class EmptyClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var table = new int[10, 10];
+            for (int i = 1; i <= n; i++)
+            {
+                table[FirstDigit(i), i % 10]++;
+            }
+
+            long result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += table[i % 10, FirstDigit(i)];
+            }
+            Console.WriteLine(result);
+        }
+
+        static int FirstDigit(int num)
+        {
+            while (num >= 10)
+            {
+                num /= 10;
+            }
+            return num;
+        }
+    }
+}
