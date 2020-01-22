74791d875c8e7d2db92f374de38460f895b5a02e
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-22 21:04:06 +0900

    ABC146AB

---
+++ ABC145/ABC145A/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ABC145A
+{
+    public class EmptyClass
+    {
+        public static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine(r * r);
+        }
+        
+    }
+}
---
+++ ABC145/ABC145B/EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ABC145B
+{
+    public class EmptyClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            if (n%2==1)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                string a = s.Substring(n / 2);
+                string res = s == a + a ? "Yes" : "No";
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
