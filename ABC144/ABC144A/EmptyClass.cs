9b73176586657dab7d17ceac4a282f28cd14ffd7
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-23 22:02:13 +0900

    ABC144AB

---
+++ ABC144/ABC144A/EmptyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ABC144A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inp = Console.ReadLine().Split(' ');
+            int a = int.Parse(inp[0]);
+            int b = int.Parse(inp[1]);
+
+            if (a > 9 || b > 9)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(a * b);
+            }
+
+        }
+    }
+}
---
+++ ABC144/ABC144B/EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ABC144B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (i * j == n)
+                    {
+                        Console.WriteLine("Yes");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
