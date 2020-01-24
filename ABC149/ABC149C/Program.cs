41f1277f6bfc39ae5f15de263c65c8f4471e8be5
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-24 22:25:51 +0900

    ABC149C

---
+++ ABC149/ABC149C/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ABC149C
+{
+    public class EmptyClass
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (isPrime(x))
+                {
+                    Console.WriteLine(x);
+                    break;
+                }
+                x++;
+            }
+        }
+
+        static bool isPrime(long n)
+        {
+            if (n < 2) return false;
+            if (n % 2 == 0 && n != 2) return false;
+            for (int i = 3; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
