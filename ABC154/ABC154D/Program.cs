c76ec107e1503adb51ef327bc552000118e8fbd6
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-10 23:20:57 +0900

    ABC154D

--- ABC154/ABC154D/Program.cs
+++ ABC154/ABC154D/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
-using System.Linq;
+
 namespace ABC154D
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             int[] nk = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            int[] p = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            long[] p = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+            double[] sums = new double[nk[0] + nk[1]];
             double temp = 0;
-            for (int i = 0; i < nk[0]-nk[1]+1; i++)
+            for (int i = 0; i < nk[0]; i++)
             {
-                if (i !=0 && p[i-1]>= p[i+nk[1]-1])
-                {
-                    continue;
-                }
-                double sum = ((double)p.Skip(i).Take(nk[1]).Sum() / 2) + (0.5*nk[1]);
-                if (sum>temp)
-                {
-                    temp = sum;
-                }
-                
+                sums[i] = temp + ((double)(p[i] + 1) / 2);
+                temp = sums[i];
             }
-            Console.WriteLine(temp);
+
+            double res = sums[0 + nk[1] - 1];
+            for (int i = 1; i < nk[0]; i++)
+            {
+                res = Math.Max(res, sums[i + nk[1] - 1] - sums[i - 1]);
+            }
+            Console.WriteLine(res);
         }
     }
 }
