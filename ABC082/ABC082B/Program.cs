eaa1e3281d73b40fdb98716945c1e81d2939ba80
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-13 21:56:35 +0900

    0213

---
+++ ABC082/ABC082B/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            int[] sInt = s.ToCharArray().Select(e => (int)e).OrderBy(e => e).ToArray();
+            int[] tInt = t.ToCharArray().Select(e => (int)e).OrderByDescending(e => e).ToArray();
+            string res = string.Empty;
+            int length = s.Length > t.Length ? t.Length : s.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (sInt[i] == tInt[i])
+                {
+                    continue;
+                }
+                else if (sInt[i] > tInt[i])
+                {
+                    res = "No";
+                    break;
+                }
+                else if (sInt[i] < tInt[i])
+                {
+                    res = "Yes";
+                    break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(res))
+            {
+                res = s.Length >= t.Length ? "No" : "Yes";
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ AGC006/AGC006A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            int dupCnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t.Substring(0, i + 1) == s.Substring(n - (i + 1)))
+                {
+                    dupCnt = Math.Max(dupCnt, i + 1);
+                }
+            }
+            Console.WriteLine(s.Length + t.Length - dupCnt);
+
+
+        }
+    }
+}
