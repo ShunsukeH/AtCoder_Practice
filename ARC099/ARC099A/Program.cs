5ebe03abf9c1e1e78762cdcd7a19418e40df1d66
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-16 10:37:01 +0900

    ARC099A

---
+++ ARC099/ARC099A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC099A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] nk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nk[0]-nk[1];
+            int cnt = 1;
+            while (n > 0)
+            {
+                n = (n + 1) - nk[1];
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
