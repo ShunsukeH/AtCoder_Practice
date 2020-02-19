6f9084ab7d9dc49adcf7e13951aa89267696e6ab
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-19 23:47:22 +0900

    ABC134AB

---
+++ ABC134/ABC134A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ABC134A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine(3 * r * r);
+        }
+    }
+}
---
+++ ABC134/ABC134B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC134B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] nd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int checkable = (nd[1] * 2) + 1;
+            Console.WriteLine(Math.Ceiling((double)nd[0] / checkable));
+        }
+    }
+}
