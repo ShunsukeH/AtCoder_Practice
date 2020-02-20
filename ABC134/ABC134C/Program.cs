a4569be5383afd9f91b51cbe90f935de6c89656b
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-20 09:22:00 +0900

    ABC134C

---
+++ ABC134/ABC134C/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ABC134C
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int max = 0;
+            int secound = 0;
+            int maxCnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                if (a[i] == max) maxCnt++;
+                else if (a[i] > max)
+                {
+                    secound = max;
+                    maxCnt = 1;
+                    max = a[i];
+                }
+                else
+                {
+                    secound = Math.Max(secound, a[i]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] < max)
+                {
+                    Console.WriteLine(max);
+                }
+                else if(a[i] == max)
+                {
+                    Console.WriteLine(maxCnt > 1 ? max : secound);
+                }
+            }
+        }
+    }
+}
