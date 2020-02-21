5211bd5fbd7efc52e7d872be8218db3cab80ae2c
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-21 23:06:17 +0900

    ABC134A

---
+++ ABC133/ABC133A/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC133A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] nab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(nab[0] * nab[1] > nab[2] ? nab[2] : nab[0] * nab[1]);
+        }
+    }
+}
