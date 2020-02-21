061fd474782a4c77927821abb4034af10c93019f
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-22 00:15:44 +0900

    ABC133B

---
+++ ABC133/ABC133B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC133B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
