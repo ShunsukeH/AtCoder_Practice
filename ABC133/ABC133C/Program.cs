b7f468f5c33952bb2ab7482bff3c5aec90ac6685
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-03-07 10:36:00 +0900

    ABC133C

---
+++ ABC133/ABC133C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC133C
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            long[] lr = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long res = 2020;
+
+            if (lr[1]-lr[0]>=2018)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (long i = lr[0]; i <= lr[1]; i++)
+            {
+                for (long j = i+1; j <= lr[1]; j++)
+                {
+                    long tmp = (i * j) % 2019;
+                    res = Math.Min(res, tmp);
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
