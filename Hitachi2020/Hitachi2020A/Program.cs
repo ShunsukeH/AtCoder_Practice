e04421bd665cd05a4dcb5d097669a393dcf5fe05
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-03-14 19:15:21 +0900

    hitachi

---
+++ Hitachi2020/Hitachi2020A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Hitachi2020A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string res = "Yes";
+            if (s.Length%2 == 1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s.Substring(i,2) == "hi")
+                {
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    res = "No";
+                    break;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ Hitachi2020/Hitachi2020B/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Hitachi2020B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] abm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int min = a.Min() + b.Min();
+            int[][] ms = new int[abm[2]][];
+
+            for (int i = 0; i < abm[2]; i++)
+            {
+                // ms[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var m = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                ChangeMin(ref min, a[m[0]-1] + b[m[1]-1] - m[2]);
+            }
+
+            Console.WriteLine(min);
+        }
+
+        static bool ChangeMin(ref int a, int b)
+        {
+            if (a > b)
+            {
+                a = b;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
